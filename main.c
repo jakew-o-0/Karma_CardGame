dc95aceda0bbb2c0ebdff3daba20fd57c6e4e471
jakew-o-0 <jakewbailey007@gmail.com>
2023-01-29 00:57:26 +0000

    ???

--- main.c
+++ main.c
@@ -32,9 +32,14 @@ typedef struct table_hand table_hand_t ;
 
 void push(node_t** head, card_t* new_card);   // pushes the newest node to the head of the list
 void remove_node(node_t** head, int pos);   //removes a node by value
+
 void generate_rand_hand(node_t** hand, int deck[4][13]);
 void shuffle_deck(int deck[4][13]);
 void generate_table_hand(table_hand_t* hand, int deck[4][13]);
+void initialise_table(card_t* table);
+card_t random_card(int deck[4][13]);
+void print_list(node_t* head);
+void select_hand(node_t* head, table_hand_t* up_hand[3], table_hand_t* down_hand[3]);
 
 
 
@@ -54,15 +59,25 @@ int main(int argc, char** argv)
                           {1,2,3,4,5,6,7,8,9,10,11,12,13},
                           {1,2,3,4,5,6,7,8,9,10,11,12,13}};
 
+      //generate linked list heads for player and computer hands
+      //
       node_t* player_hand = (node_t*)malloc(sizeof(node_t));
       node_t* computer_hand = (node_t*)malloc(sizeof(node_t));
 
+
       shuffle_deck(&deck);
 
+
+      //generate random hands for computer and player
+      //
       generate_rand_hand(&player_hand, &deck);
       generate_rand_hand(&computer_hand, &deck);
 
 
+      //generate random table hands for computer and player
+      //up_hands will be visible to both player and computer
+      //down hands will be invisible to both
+      //
       table_hand_t player_down_hand[3];
       table_hand_t player_up_hand[3];
       table_hand_t computer_down_hand[3];
@@ -74,25 +89,71 @@ int main(int argc, char** argv)
       generate_table_hand(&computer_up_hand, &deck);
 
 
-      for(int i = 0; i < 4; i++)
-      {
-            printf("%d\n", i);
-            for(int j = 0; j < 13; j++)
-            {
-                  printf("%d, ", deck[i][j]);
-            }
-            printf("\n");
-      }
+      //create a random starting card
+      //
+      card_t table = random_card(&deck);
 
+
+
+
+      
+      //main game loop
+      //
+      int current_player = 0;      //0 = player, 1 = computer
       while(1)
       {
-           break; 
+            printf("your hand:\n");
+            if(~(current_player))
+            {
+                  select_hand(player_hand, &player_up_hand, &player_down_hand);
+            }
+            else
+            {
+                  print_list(computer_hand);
+            }
+
+            return 0;
+
       }
 
 
 
       return 0;
 }
+
+
+
+
+
+
+
+
+void select_hand(node_t* head, table_hand_t* up_hand[3], table_hand_t* down_hand[3])
+{
+      int total_up = 0;
+      for(int i = 0; i < 3; i++)
+      {
+            total_up += up_hand[i]->is_playable;
+      }
+
+      if(head != NULL)
+      {
+            print_list(head);
+      }
+      else
+      {
+            if(total_up > 0)
+            {
+                  //use up_hand
+            }
+            else
+            {
+                 //use down_hand 
+            }
+      }
+
+}
+
 
 
 
@@ -170,6 +231,33 @@ void push(node_t** head, card_t* new_card)
 
 
 
+////////////////////////////////////////////////////////////
+
+
+
+
+void print_list(node_t* head)
+{
+      node_t* curent = head;
+      while(curent->next != NULL)
+      {
+            printf("%d: ", curent->position);
+
+            switch (curent->card.suit) 
+            {
+                  case 0: printf("heart, ");
+                  case 1: printf("clubs, ");
+                  case 2: printf("diamonds, ");
+                  case 3: printf("spades, ");
+            }
+
+            printf("%d\n", curent->card.type);
+      }
+}
+
+
+
+
 
 
 ////////////////////////////////////////////////////////////
@@ -184,19 +272,7 @@ void generate_rand_hand(node_t** hand, int deck[4][13])
 
       for(int i = 0; i < 3; i++)
       {
-            int rand_suit = rand() % 4;            //create a random suit
-
-            int j = 0;
-            while(deck[rand_suit][j] == 0)         //find the first non-0 number
-            {
-                  j++;
-            }
-
-            new_card.type = deck[rand_suit][j];    //add the new cards suit and type
-            new_card.suit = rand_suit;
-
-            deck[rand_suit][j] = 0;                //set the curent number 0 to prevent duplicate cards
-
+            new_card = random_card(deck);
             push(hand, &new_card);                 //add the new card to a node in the given hand
       }
 }
@@ -233,22 +309,30 @@ void shuffle_deck(int deck[4][13])
 
 void generate_table_hand(table_hand_t* hand, int deck[4][13])
 {
-      srand(time(NULL));
-
       for(int i = 0; i < 3; i++)
       {
-            int rand_suit = rand() % 4;            //create a random suit
-
-            int j = 0;
-            while(deck[rand_suit][j] == 0)         //find the first non-0 number
-            {
-                  j++;
-            }
-
             hand[i].is_playable = 1;
-            hand[i].card.suit = rand_suit;
-            hand[i].card.type = deck[rand_suit][j];
-
-            deck[rand_suit][j] = 0;
+            hand[i].card = random_card(deck);
       }
+}
+
+
+
+/////////////////////////////////////////////////////////////
+
+card_t random_card(int deck[4][13])
+{
+      srand(time(0)); 
+      int rand_suit = rand() % 4;            //create a random suit
+
+      int j = 0;
+      while(deck[rand_suit][j] == 0)         //find the first non-0 number
+      {
+            j++;
+      }
+
+      deck[rand_suit][j] = 0;
+
+      card_t rand_card = {j,rand_suit};
+      return rand_card;
 }
